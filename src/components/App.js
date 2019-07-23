b01db048010576a8217a11fc6c353630a287e7fb
Emeka Augustine <cafafanscoders@gamil.com>
2019-07-22 22:46:49 -0400

    Install semantic-ui-react and semantic-ui-css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        App
+        <Header />
       </div>
     );
   }
